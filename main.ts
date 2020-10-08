1cc2fb23b0e25458c0a62207f77c85a4ffb5c942
msgumtow <71787320+msgumtow@users.noreply.github.com>
2020-10-08 13:55:15 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+/**
+ * Forever:
+ * 
+ * If button A is pressed then start melody (your choice) and show an icon (your choice); else clear screen.
+ * 
+ * If button B is pressed then start melody (your choice, choose a different one) and show an icon (your choice again), else clear screen.
+ */
