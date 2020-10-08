d91020e83562047547b2cd3ec094f7aa5a722ecf
msgumtow <71787320+msgumtow@users.noreply.github.com>
2020-10-08 13:56:21 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,4 +4,6 @@
  * If button A is pressed then start melody (your choice) and show an icon (your choice); else clear screen.
  * 
  * If button B is pressed then start melody (your choice, choose a different one) and show an icon (your choice again), else clear screen.
+ * 
+ * REMEMBER: If/then/else blocks are in the logic section. Look for a HEXAGONAL "button A/B" pressed" block in input.
  */
